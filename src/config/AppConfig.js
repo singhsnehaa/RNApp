bc78c7eae7b283b05cb7000e6617144db2f9fb24
singhsnehaa <66714324+singhsnehaa@users.noreply.github.com>
2021-01-06 23:48:14 +0530

    Config created

---
+++ src/config/AppConfig.js
@@ -0,0 +1,17 @@
+export const AppConfig = {
+    API: 'https://adxpressindia.com/app/api/call',        //online
+    //API: 'http://192.168.43.112/adxpressindia/api/call',      //Local Home
+    //API: 'http://192.168.29.112/adxpressindia/api/call',      //Local Office
+    GOOGLE_API_KEY: 'AIzaSyDWdZFRTchik8-aeq_cCRiUNBvCrozXido',
+    INR_SYMBOL: '\u20B9',
+    RAZOR_PAY_ID: 'rzp_live_ToEJim2M20PnZw',
+    RAZOR_PAY_KEY_SECRET: 'UdpyzpTMDh4HFMzhFWAc7sCK',
+    ONE_SIGNAL_API_ID: '6d98fb9c-6b18-409f-a0ad-26042de06297',
+    ONE_SIGNAL_API_KEY: 'MDk4YzAxZmYtYmNjMy00NzcwLWI4Y2YtYzc2ODIyYWVhZTMw',
+};
+
+export const Support = {
+    WhatsApp: 'http://api.whatsapp.com/send?phone=7004780441,',
+    Email: 'adxpress17@gmail.com',
+    Call: 'tel:7004780441',
+  };
